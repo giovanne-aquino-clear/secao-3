c90bdd8a8807f0012ce41f613fb0f267af849a55
giovanne-aquino-clear <giovanneaquino15@gmail.com>
2023-08-06 15:27:27 -0300

    Transfere document.getElementById para a função onSubmit

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -4,6 +4,40 @@ class UserController{
     }
 
     onSubmit(){
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    fields.forEach(function(field, index){
+
+        if (field.name === "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value
+            }
+
+        } else {
+
+            user[field.name] = field.value
+
+        }
+
+    });
+    
+    var objetctUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin,
+    );
+
+    addLine(objectUser)
+
+});
 
 
     }
--- curso-javascript-projeto-usuarios/index.js
+++ curso-javascript-projeto-usuarios/index.js
@@ -21,37 +21,3 @@ function addLine(dataUser) {
 
 }
 
-document.getElementById("form-user-create").addEventListener("submit", function(event){
-
-    event.preventDefault();
-
-    fields.forEach(function(field, index){
-
-        if (field.name === "gender") {
-
-            if (field.checked) {
-                user[field.name] = field.value
-            }
-
-        } else {
-
-            user[field.name] = field.value
-
-        }
-
-    });
-    
-    var objetctUser = new User(
-        user.name,
-        user.gender,
-        user.birth,
-        user.country,
-        user.email,
-        user.password,
-        user.photo,
-        user.admin,
-    );
-
-    addLine(objectUser)
-
-});
