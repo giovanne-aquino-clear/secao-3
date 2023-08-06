9de811b50c247e06b347c3903ad5bed482701de6
giovanne-aquino-clear <giovanneaquino15@gmail.com>
2023-08-06 14:54:02 -0300

    adiciona scripi do Users.js no html. adiciona constructor na classe User

--- curso-javascript-projeto-usuarios/Models/Users.js
+++ curso-javascript-projeto-usuarios/Models/Users.js
@@ -1,3 +1,17 @@
-class User {
+class User   {
      
+    constructor(name,gender,birth,country,email,password,photo,admin){
+            this.name = name;
+            this.gender = gender;
+            this.birth = birth;
+            this.country = gender;
+            this.email = country;
+            this.password = password;
+            this.photo = photo;
+            this.admin = admin;
+
+
+
+    }
+
 }
