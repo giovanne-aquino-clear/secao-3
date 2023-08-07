7df4bf4e15d6ba98473cfb13619936acfc9973b6
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-07 10:24:01 -0300

    corrige erro no appendChild

--- curso-javascript-projeto-usuarios/controllers/UserControllers.js
+++ curso-javascript-projeto-usuarios/controllers/UserControllers.js
@@ -114,24 +114,22 @@ class UserController {
     addLine(dataUser) {
 
         let tr = document.createElement('tr');
-
         tr.innerHTML = `
         <tr>
-        <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    </tr>
-`;
+            <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
 
-        this.tableEl.innerHTML = 
-           
 
+        this.tableEl.appendChild(tr);
     }
 
 
