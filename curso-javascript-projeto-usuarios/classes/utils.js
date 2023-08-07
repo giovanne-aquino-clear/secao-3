b3931dd25de78dab017d4074a50ea0ec86108759
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-07 11:18:58 -0300

    corrige erro de sintaxe no no return do dateformat

--- curso-javascript-projeto-usuarios/classes/utils.js
+++ curso-javascript-projeto-usuarios/classes/utils.js
@@ -3,7 +3,7 @@ class utils {
 
 static dateformat(date){
 
-    return date.getDate()+'/'+(date.getMonth()+1)+date.getFullYear()+' '+ date.getHours()+  ':' + date.getMinutes();
+    return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' '+ date.getHours()+  ':' + date.getMinutes();
 }   
 
 }
