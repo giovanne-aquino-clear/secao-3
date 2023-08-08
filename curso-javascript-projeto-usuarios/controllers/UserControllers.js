f42c1655ce15d35545094d8754b0645515288e2d
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-08 08:19:29 -0300

    cria metodo showPanel Create, showPanelUpdate e onEdit, para realizar a edição dos dados do usario

--- curso-javascript-projeto-usuarios/controllers/UserControllers.js
+++ curso-javascript-projeto-usuarios/controllers/UserControllers.js
@@ -5,7 +5,18 @@ class UserController {
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
-        this.onSubmit()
+        this.onSubmit();
+        this.onEdit();
+
+    }
+
+    onEdit(){
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+            this.showPanelCreate();
+
+        });
 
     }
 
@@ -138,26 +149,47 @@ class UserController {
         tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
-            
+            <tr>
                 <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-                <td>${ utils.dateFormat(dataUser.register)}</td>
+                <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-           
+            </tr>
         `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+
+        });
 
         this.tableEl.appendChild(tr);
 
-        this.updateCount()
+        this.updateCount();
 
     }
 
-    updateCount() {
+    showPanelCreate(){
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+
+    }
+
+    updateCount(){
 
         let numberUsers = 0;
         let numberAdmin = 0;
